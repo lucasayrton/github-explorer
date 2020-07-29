e6165ff896a28843c13a0faf04f4f0ad1132bca8
Lucas Ayrton Marchi Soares <lucasayrton66@gmail.com>
2020-07-29 00:59:12 -0300

    Updates

---
+++ precache-manifest.6d4ac144b445314ac4990832e3fb6342.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab523d102e0acb16c281bafa60c53ba",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "0c36d960c91fcdb6a3e3",
+    "url": "/github-explorer/static/js/2.03b69829.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.03b69829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db83b1edef63bfb0d00",
+    "url": "/github-explorer/static/js/main.f874b9ee.chunk.js"
+  },
+  {
+    "revision": "e2f0772bece6068ae40c",
+    "url": "/github-explorer/static/js/runtime-main.906dbdf0.js"
+  },
+  {
+    "revision": "88c2d35eff7ebd12dc82cba71327f4bf",
+    "url": "/github-explorer/static/media/github-background.88c2d35e.svg"
+  },
+  {
+    "revision": "18e060ecceda949068ce519ae6d8b261",
+    "url": "/github-explorer/static/media/logo.18e060ec.svg"
+  }
+]);
